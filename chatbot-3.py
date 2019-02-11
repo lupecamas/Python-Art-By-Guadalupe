ca1c24d86631a1bf9f0e20b9aa0b6a2c9a2c8b36
lupecamas <46791315+lupecamas@users.noreply.github.com>
2019-02-11 09:58:56 -0700

    Add files via upload

---
+++ chatbot-3.py
@@ -0,0 +1,71 @@
+print (' Hello my name is Rick. What is your name?')
+name = input ()
+print ("Hello " + name +", nice to meet you.")
+print ( "Do you watch Rick and Morty?")
+print ('yes or no')
+answer = input().lower()    
+if answer == "yes":
+    print("Let talk about that.")
+    print("Who do you think evil Morty is?")
+    print('---------------------------------')
+    print ('A.Evil Morty is the original Morty?')
+    print ('B.Is evil Morty just out the ricks?')
+    print('------------------------------------')
+   
+    
+else:
+    print ("Well do you like anime? Answer yes or no.")
+    
+question1  = input()
+if question1 == ("YES" or "Yes" or "yes"):
+    print("Lets talks about that.")
+    
+
+    
+    
+    
+    
+answer4 = input()
+if Question  == "a" or "A":
+    print('I think so too.I think evil Morty wants revenge on Rick for using him.')
+    print('I think Morty realize that Rick only cares about himself')
+else:
+    print ('I think that killing the Ricks is part of his greater plans.')
+    
+
+
+    print("--------------------------")
+    print('----------------------------------')
+print ('Who do you think well be stepping up in the up coming season?')
+print('----------------------------------')
+print('C. Jerry')
+print('D.Summer')
+answer1 = input ()
+if answer1 == ("C" or "c"):
+    print("I think we well see Jerry grow in the coming season.")
+    print("Maybe we well see Beth and him faling back in love.")
+else:
+    print("I think summer is going to be a big part in season 4.")
+        
+    
+
+
+
+
+answer = input()
+if answer == ("YES" or "Yes" or "yes"):
+    print( "Do you think Saitama looks good with out his hair?")
+    print('-----------------------')
+    print("A.yes")
+    print("B.no")
+    print('-----------------------')
+    answer =input()
+if answer == "yes":
+    print( "He be looking like meal")
+
+else:
+    print (" I don't agree with you :(.") 
+    
+
+ 
+
